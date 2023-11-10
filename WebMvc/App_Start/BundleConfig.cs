54e2d5fe00143eab7dba8f962b611a2ae05bf846
Yamil Font <Yamil@imfont.com>
2023-11-09 21:12:39 -0800

    working template with updates styles

--- WebMvc/App_Start/BundleConfig.cs
+++ WebMvc/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace WebMvc
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/root.css",
                       "~/Content/site.css"));
         }
     }

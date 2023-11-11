117d3380a042b0032fdb954e354e5897381b19f1
Yamil Font <Yamil@imfont.com>
2023-11-11 10:16:51 -0800

    New layout files to handle multiple columns, added profile page with test data

---
+++ WebMvc/Controllers/ProfileController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using WebMvc.ViewModels.Profile;
+
+namespace WebMvc.Controllers
+{
+    [RoutePrefix("Profile")]
+    public class ProfileController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            var model = new MyProfileIndexViewModel()
+            {
+                UserGuid = new System.Guid(),
+                GivenName = "Alex",
+                MiddleName = "Jordan",
+                Surname = "Smith",
+                Biography = "Alex is a dedicated professional with over 10 years of experience in software development. Known for innovative problem-solving skills and a passion for technology.",
+                ManagerName = "Taylor Morgan",
+                Title = "Senior Software Developer",
+                Division = "Division of Information Technology",
+                Branch = "Application Development",
+                PhoneNumber = "(555) 123-4567",
+                EmailAddress = "alex.smith@example.com"
+        };                       
+            
+            return View(model);
+        }
+    }
+}
---
+++ WebMvc/ViewModels/Profile/MyProfileIndexViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WebMvc.ViewModels.Profile
+{
+    public class MyProfileIndexViewModel
+    {
+        public Guid UserGuid { get; set; }
+        public string GivenName { get; set; }
+        public string MiddleName { get; set; }
+        public string Surname { get; set; }
+        public string Biography { get; set; }
+        public string ManagerName { get; set; }
+        public string Title { get; set; }
+        public string Division { get; set; }
+        public string Branch { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
